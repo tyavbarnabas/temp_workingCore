3bcbe24f671539d1ed6c6aaafde2d1eea341da6f
beny1122war <tyavbarnabas2@gmail.com>
2024-05-17 13:06:21 +0100

    made fixes to the jwt

--- app/src/main/java/com/kenpb/app/menu/Menu.java
+++ app/src/main/java/com/kenpb/app/menu/Menu.java
@@ -23,33 +23,5 @@ public class Menu {
     @NotNull
     private String name;
 
-
     private String state;
-
-//    @NotNull
-//    private int archived=0;
-//
-//    private String type ="LINK";
-//
-//    @NotNull
-//    private String level = "LEVEL_1";
-//
-//    @NotNull
-//    private Integer position = 1;
-//
-//    private String icon;
-//
-//    @Column(name = "code")
-//    private String code;
-//
-//    private String uuid;
-//
-//    private String tooltip;
-//
-//    private String breadcrumb;
-//
-//    private String url;
-//
-//    private Boolean disabled = false;
-
 }
--- app/src/main/java/com/kenpb/app/security/JwtAuthFilter.java
+++ app/src/main/java/com/kenpb/app/security/JwtAuthFilter.java
@@ -34,10 +34,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
 
-//        if (request.getServletPath().contains("/api/v1/auth")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
 
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
